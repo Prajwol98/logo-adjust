e8f7c983e37a2f0ed139709e5d614d4711916a13
Prajwol Mainali <prajwolmainali96@gmail.com>
2024-07-13 09:28:52 +0545

    init repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="sm:bg-red-800 md:bg-green-300 bg-yellow-400">
+        hello programmers of the world what is the best things that happend
+        today in th
+      </p>
+      ;<h1 className="text-red-200">hello man</h1>
+    </>
+  );
+}
+
+export default App;
