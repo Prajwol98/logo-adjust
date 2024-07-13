b8e79849ac4da82ef1fa0f8977c38049ddd4bd14
Prajwol Mainali <prajwolmainali96@gmail.com>
2024-07-13 21:26:21 +0545

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
-      <p className="sm:bg-red-800 md:bg-green-300 bg-yellow-400">
-        hello programmers of the world what is the best things that happend
-        today in th
-      </p>
-      ;<h1 className="text-red-200">hello man</h1>
+      <Button>click me</Button>
     </>
   );
 }
