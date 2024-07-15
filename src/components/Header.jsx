0ba2815ed436f89faab6393973d8c4eeb9e73d2c
Prajwol Mainali <prajwolmainali96@gmail.com>
2024-07-15 18:17:50 +0545

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,13 @@ import { Download } from "lucide-react";
 
 const Header = () => {
   return (
-    <div className="flex items-center ">
-      <img src="logo.svg" alt="logo" className="h-20 w-20" />
+    <div className="p-2 flex items-center justify-between border-b-2 border-black">
+      <div className="flex items-center">
+        <img src="logo.svg" alt="logo" className="h-[50px] w-[50px]" />
+        <span className="text-[20px] font-bold ">Logo Adjust </span>
+      </div>
       <Button className="bg-destructive">
-        <Download></Download>Download
+        <Download className="mr-2"></Download>Download
       </Button>
     </div>
   );
